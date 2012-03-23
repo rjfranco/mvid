832b8d60d02d46457232faeed45e3922c5d12637
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-03-23 15:43:25 -0700

    Adding initial copy of application from Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mvid::Application.config.secret_token = '296b43ab9733cf1e7f6096527bdd42392ad427880bd74f7f576dad4e3b09d0930565093f0fd53e4817ed9c0a3231a270b1286b580bcd9c854c45c3a49edb9dfd'
